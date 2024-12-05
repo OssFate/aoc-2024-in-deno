5a11d944c363cb1ad0198e3f0be8b62818eb40fc
OssFate <mantis1512@gmail.com>
2024-12-04 21:51:36 -0500

    Added some error control on execution

--- aoc.ts
+++ aoc.ts
@@ -1,9 +1,22 @@
 import { days } from "./day/days.ts";
 
 const day = Deno.args[0];
+if (day == undefined) {
+  console.error(
+    `Missing day argument, please use like "deno task day <day you want to run>"`,
+  );
+  Deno.exit();
+}
+
+const lowerDay = day.toLowerCase();
+if (!(lowerDay in days)) {
+  console.error(`"${day}" is a missing day for now...`);
+  Deno.exit();
+}
+
+const oneSolution = await days[lowerDay].one();
+const twoSolution = await days[lowerDay].two();
 
 console.log(`Running day: ${day}`);
-console.log(`Day ${day} first problem: ${await days[day.toLowerCase()].one()}`);
-console.log(
-  `Day ${day} second problem: ${await days[day.toLowerCase()].two()}`,
-);
+console.log(`Day ${day} first problem: ${oneSolution}`);
+console.log(`Day ${day} second problem: ${twoSolution}`);
